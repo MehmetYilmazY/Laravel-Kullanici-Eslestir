a486327f7bc5cd1ff9d6af6f76d8b03012484c09
Sleexc <mehmetyilmazyt@gmail.com>
2023-07-26 17:45:26 +0300

    Laravel CRUD app

---
+++ laravel-kullanici-eslestirme/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('usertype')->default('user'); // Departman alanı, null değer alabilir.
+            $table->string('department')->nullable(); // Departman alanı, null değer alabilir.
+            $table->string('role')->nullable(); // Görev alanı, null değer alabilir.
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
